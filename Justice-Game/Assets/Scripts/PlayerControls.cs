553e565e0149e9b7f8037439b4e806d9a90d8741
Gabby-Orr <gorr@trinity.edu>
2020-09-24 08:18:15 -0500

    Did player controls with a power to extra jump in the air that decreases (in force and time you have left to jump in air) as you use it. When player is grounded, jump power replenishes

---
+++ Justice-Game/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public LayerMask platformMask;
+    public Rigidbody2D rb;
+    private CircleCollider2D col;
+    private float speed = 5f;
+    private float power = 10;
+    private float powerDecreaseRate = 3f;
+
+    [SerializeField] private float jumpVelo = 8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
+
+        Debug.Log(power);
+
+        if (IsGrounded())
+        {
+            power = 10;
+            jumpVelo = 8f;
+        }
+        else
+        {
+            power -= powerDecreaseRate * Time.deltaTime;
+            jumpVelo -= powerDecreaseRate * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && (IsGrounded() || power > 0))
+        {
+            rb.velocity = Vector2.up * jumpVelo;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        
+        RaycastHit2D raycastHit2D = Physics2D.CircleCast(col.bounds.center, col.radius, Vector2.down, .1f, platformMask);
+        return raycastHit2D.collider != null;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Enemy"))
+        {
+            //whatever happens here
+        }
+    }
+}
