dde468f0c3eb497e527a40cb1daaf3ee263504be
wyntrr-end <66278138+wyntrr-end@users.noreply.github.com>
2020-09-26 16:11:13 -0500

    add start menu and game over menu
    - add Breamcatcher font, used for UI
    - add Manager.cs, MainMenuManager.cs, and GameOverManager.cs to manage transitions and UI

---
+++ Justice-Game/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private int timeElapsed;
+    public Text titleText;
+    public Text titleTextShadow;
+    public Text startButtonText;
+    void Awake()
+    {
+        titleTextShadow.text = titleText.text;
+    }
+    void Update()
+    {
+        timeElapsed++;
+        if (timeElapsed < 100)
+            startButtonText.color = new Color32(237, 191, 18, 255);
+        else
+            startButtonText.color = Color.black;
+        if (timeElapsed > 200)
+            timeElapsed = 0;
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
---
+++ Justice-Game/Assets/Scripts/Manager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager instance;
+    private Manager() {}
+    public static int WIN = 1;
+    public static int LOSE = 0;
+    private bool isGameOver = false;
+    public bool IsGameOver()
+    {
+        return isGameOver;
+    }
+    private bool isWin = false;
+    public bool IsWin()
+    {
+        return isWin;
+    }
+    private void Awake()
+    {
+        if (instance)
+            Destroy(gameObject);
+        else
+            instance = this;
+    }
+    public void GameOver(int status)
+    {
+        if (!isGameOver)
+        {
+            isGameOver = true;
+            isWin = (status == WIN);
+            SceneManager.LoadScene("GameOverScene", LoadSceneMode.Additive);
+        }
+    }
+    public void RestartGame()
+    {
+        // do other things to reset the game
+        isGameOver = false;
+        SceneManager.UnloadSceneAsync("GameOverScene");
+    }
+}
